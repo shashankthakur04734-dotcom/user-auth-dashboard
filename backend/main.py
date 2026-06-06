b810fe0072ddebc4cbe4e7b60e5e1e969c34a72e
shashankthakur04734-dotcom <shashankthakur04734@gmail.com>
2026-06-06 11:50:06 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -49,7 +49,6 @@ def seed_admin():
 @app.get("/")
 def root():
     return {"message": "User Authentication API is running"}
-    
 @app.post("/api/auth/register", response_model=schemas.UserOut, status_code=status.HTTP_201_CREATED)
 def register(user_in: schemas.UserCreate, db: Session = Depends(get_db)):
     # Check if username exists
